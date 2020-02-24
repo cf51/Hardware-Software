597a7ae6d207748fa12044f64366f10532f84bff
Callum Forsyth <43421946+cf51@users.noreply.github.com>
2020-02-24 14:55:58 +0000

    Designing PPM struct

--- courseWork.c
+++ courseWork.c
@@ -1,5 +1,18 @@
+#include<stdio.h>
+
+
 // Course: Hardware-Software CW1
 // Name: Callum Forsyth
 // User: cf51 
 
-#include<stdio.h>
+
+struct PPM{
+
+    int width, height, maxColour; 
+    char comments[255]; 
+    struct Pixels *pixels; 
+}; 
+
+struct Pixels{
+    int red, green, blue; 
+};  
