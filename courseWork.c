27e5c0b42fb8829c1914b8155de7efc594985574
Callum Forsyth <43421946+cf51@users.noreply.github.com>
2020-02-24 15:05:53 +0000

    Adding in the empty structs

--- courseWork.c
+++ courseWork.c
@@ -1,18 +1,34 @@
 #include<stdio.h>
-
+#include<stdlib.h> 
 
 // Course: Hardware-Software CW1
 // Name: Callum Forsyth
 // User: cf51 
 
-
+// Structure that stores the PPM Data
 struct PPM{
-
     int width, height, maxColour; 
     char comments[255]; 
     struct Pixels *pixels; 
 }; 
 
+// Structure that stores the Pixel Data 
 struct Pixels{
     int red, green, blue; 
 };  
+
+struct PPM * getPPM(FILE * f){
+
+}
+
+showPPM(struct PPM * im){
+
+}
+
+struct PPM * encode(struct PPM * im, char * message, unsigned int mSize, unsigned int secret){
+
+}
+
+char * decode(struct ppm * im, unsigned int secret){
+
+}
