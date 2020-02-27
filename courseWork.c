24ccfb16cb996d37209034fe8df7e481d1b40e7d
Callum Forsyth <43421946+cf51@users.noreply.github.com>
2020-02-27 13:09:10 +0000

    Checking PPM file validation

--- courseWork.c
+++ courseWork.c
@@ -13,17 +13,30 @@ struct Pixels{
 
 // Structure that stores the PPM Data
 struct PPM{
-    char P3[2]; 
+    char P3[2];  
     char comments;
     int width, height, maxColour; 
     struct Pixels *pixels; 
 }; 
 
-
-
 struct PPM *getPPM(FILE * f){
 
-}
+    // Allocates dynamic memeory to PPM using malloc 
+    struct PPM *image =  (struct PPM *) malloc(sizeof(struct PPM)); 
+
+    // Checking the PPM to make sure it is P3 (correct format)
+    fgets(image->P3, sizeof(image->P3), f);
+
+    // Returning null if the PPM is not P3 
+    if ((image->P3[0] != 'P') && (image->P3[1] != '3')){
+        perror("Error not P3 format"); 
+        return NULL; 
+        
+    }
+
+
+
+}; 
 
 
 void showPPM(struct PPM * im){
